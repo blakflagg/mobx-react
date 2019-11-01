16ef15a4f32fb9e38a99cee7504cba4147135085
Dan lnx-manjaro <locus78@gmail.com>
2019-11-01 09:42:37 -0700

    added an action

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -5,6 +5,16 @@ import { inject, observer } from 'mobx-react';
 @inject('store')
 @observer
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { isToggleOn: true };
+
+    }
+    changeMSG = () => {
+        console.log(this.props)
+        this.props.store.changeMSG("This is a new message");
+    }
+
     render() {
         const { store } = this.props;
         console.log('store = ' + store);
@@ -14,6 +24,11 @@ class Home extends React.Component {
                     HOME
                 {store.pageMSG}
                 </h1>
+                <div className="btnUpdate">
+                    <button onClick={this.changeMSG}>
+                        Update the message here
+                    </button>
+                </div>
             </div>
         )
     }
--- src/stores/appStore.js
+++ src/stores/appStore.js
@@ -1,12 +1,16 @@
-import { observable, computed, action} from 'mobx';
+import { observable, computed, action } from 'mobx';
 
 export class appStore {
 
 
     @observable pageMSG = "this is page1";
-   
+
     get pageMSG() {
         return this.pageMSG;
+    }
+
+    @action changeMSG(newMSG) {
+        this.pageMSG = newMSG;
     }
 }
 
